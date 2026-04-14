5726c4383aba336b53a350d0da1230e2dd5e95c8
candi208 <erandiyoan.candia@upaep.mx>
2026-04-14 07:43:46 -0600

    Create Inventario.cs

---
+++ Inventario.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+
+public class Inventario
+{
+    public int ContadorID {get; set;}
+    
+    List<Producto>productos=new List<Producto>();
+    
+    public void AgregarProducto()
+    {
+        
+    }
+    
+    public void ListarProductos()
+    {
+        
+    }
+    
+    public void BuscarProducto()
+    {
+        
+    }
+    
+    public void ActualizarStock()
+    {
+        
+    }
+    
+    public void EliminarProducto()
+    {
+        
+    }
+}
+ 
+ public class Producto
+ {
+     public int Id {get ; set;}
+     public string Nombre{get; set;}
+     public int Stock{get; set;}
+ }
+
+class Program
+{
+    static void Main()
+    {
+       
+    }
+}
